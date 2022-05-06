5f4850d679c4ac536cd047d28ae45359c0139d96
Gabriel <gabeguimaraes13@gmail.com>
2022-05-06 15:28:59 -0300

    add scrolltop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useApp } from "hooks/app";
-import { Box } from "@mui/material";
+import { Box, Fab, Toolbar } from "@mui/material";
 import { Home } from "pages/home/Home";
 import { makeStyles } from "@material-ui/core";
 import { Content } from "pages/content/Content";
@@ -8,6 +8,8 @@ import { Navbar } from "components/navbar/Navbar";
 import { Sidebar } from "components/sidebar/Sidebar";
 import { NavFooter } from "components/footer/Footer";
 import { NavbarApp } from "components/navbar/NavbarApp";
+import { ScrollTop } from "components/scrollToTop/ScrollTop";
+import { KeyboardArrowUp } from "@mui/icons-material";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -34,9 +36,19 @@ const Index: React.FC = () => {
         marginTop={app.windowWidth < 1280 ? 5 : 0}
       >
         {app.windowWidth >= 1280 && <Navbar />}
+        <Toolbar
+          sx={{
+            marginTop: -10,
+          }}
+          id="back-to-top-anchor"
+        />
         <Home />
         <Content />
-        <NavFooter />
+        <ScrollTop>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUp />
+          </Fab>
+        </ScrollTop>
       </Box>
     </div>
   );
